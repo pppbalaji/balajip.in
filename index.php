95bab0c0c020257bcdb0014f327ced8c97a575b0
Balaji <balaji.nsitd@gmail.com>
2014-12-08 00:46:25 +0530

    balajip.in first commit

---
+++ index.php
@@ -0,0 +1,422 @@
+
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <title>Balaji Palani | Resume </title>
+        <meta name="description" content="Description of your Site">
+        
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+
+		<link rel="stylesheet" href="owl-carousel/owl.carousel.css">
+        <link rel="stylesheet" href="owl-carousel/owl.theme.css">
+ 
+		<link rel="stylesheet" href="css/magnific-popup.css">
+		
+		<link rel="stylesheet" href="css/animate.css">
+		
+        <link rel="stylesheet" href="css/main.css">
+
+		<link rel="stylesheet" href="css/colorscheme-1.css">
+		
+		<link href='http://fonts.googleapis.com/css?family=Montserrat:400,700%7cOpen+Sans:100,200,300,400,700' rel='stylesheet' type='text/css'> 
+
+        <script src="js/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+        
+        <script src="http://maps.google.com/maps/api/js?sensor=false"></script>
+        
+        
+
+    </head>
+    
+    <body>
+    <!--Preloader-->	
+	<div id="preloader"><div id="spinner_container"><div class="company"><div class="border">BALAJI PALANI <img src="img/spinner.gif" alt="" /></div></div></div></div>
+	
+	<!--<nav class="navbar navbar-custom navbar-fixed-top static-background" role="navigation">-->
+	<nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
+		<div class="container">
+			<div class="navbar-header page-scroll">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
+					<i class="fa fa-bars"></i>
+				</button>
+			<div class="company"><div class="border">BALAJI PALANI</div></div>
+			</div>
+			<div class="collapse navbar-collapse navbar-right navbar-main-collapse">
+				<ul class="nav navbar-nav">				
+					<li><a href="#welcome">Welcome</a></li>
+					<li><a href="#cv">Curriculum</a></li>
+					
+					<li><a href="#contact">Contact</a></li>				
+				</ul>
+			</div>				
+		</div>			
+	</nav>
+
+	<!--main body-->
+	<section id="head">
+		<!--style 1-->		
+		<div class="container">
+			<div class="row">
+				<div class="hero" data-start="top:0px;opacity:1" data-top-bottom="top:500px;opacity:0">
+					<div class="col-sm-8 col-sm-offset-2">
+						<h1>Hi, I'm Balaji Palani</h1>
+						<div id="carousel-head" class="carousel slide carousel-fade">
+							<div class="carousel-inner">
+								<div class="item active">
+									<h3>Software Developer</h3>
+								</div>
+								
+								<div class="item">
+									<h3>From Chennai India.</h3>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>		
+		<div id="slides">
+			<div class="slides-container">
+				<img src="img/bg-head-1.jpg" alt="" />
+				<img src="img/bg-head-2.jpg" alt="" />
+			</div>
+		</div>
+	</section>
+
+	<section id="welcome">	
+		<img alt="" class="thumb-middle" src="img/user-2.jpg">			
+		<div class="container">
+			<div class="row">
+				<div class="hero col-sm-6 col-sm-offset-3">
+					<h2>Balaji Palani </h2>
+					<p>Frontend . Backend . Developer. </p>
+					<div class="space"></div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="hero col-sm-6 col-sm-offset-3">
+					<p>Short facts about me</p>
+				</div>
+			</div>
+			<div class="row">
+					<div class="col-md-6">						
+						<div class="introduction-table">
+							<div class="row">
+								<div class="col-sm-3">
+									<h4>Profile</h4>
+								</div>
+								<div class="col-sm-9">
+									<p>Software Developer . Python</p>
+								</div>
+								</div>
+								<div class="row">
+								<div class="col-sm-3">
+									<h4>Social Profiles</h4>
+								</div>
+								<div class="col-sm-9">
+										<a href="http://faceboo.com/pppbalaji"><i class="fa fa-facebook fa-fw"></i></a>
+										
+										<a href="http://in.linkedin.com/pub/balaji-palani/62/662/439/"><i class="fa fa-linkedin fa-fw"></i> </a>
+										
+										<a href="#"><i class="fa fa-dropbox fa-fw"></i></a>
+										
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-sm-3">
+									<h4>Skills</h4>
+								</div>
+								<div class="col-sm-8">
+									<div class="skillbar clearfix " data-percent="70%">
+										<div class="skillbar-title" style="background:#2F3238;"><span>PYTHON</span></div>
+										<div class="skillbar-bar" style="background:#2F3238;"></div>
+										<div class="skill-bar-percent">70%</div>
+									</div> <!-- End Skill Bar -->
+									<div class="skillbar clearfix " data-percent="90%">
+										<div class="skillbar-title" style="background:#2F3238;"><span>C</span></div>
+										<div class="skillbar-bar" style="background:#2F3238;"></div>
+										<div class="skill-bar-percent">90%</div>
+									</div> <!-- End Skill Bar -->
+									
+									<div class="skillbar clearfix " data-percent="30%">
+										<div class="skillbar-title" style="background:#2F3238;"><span>DJANGO</span></div>
+										<div class="skillbar-bar" style="background:#2F3238;"></div>
+										<div class="skill-bar-percent">30%</div>
+									</div> <!-- End Skill Bar -->
+									<div class="skillbar clearfix " data-percent="80%">
+										<div class="skillbar-title" style="background:#2F3238;"><span>HTML</span></div>
+										<div class="skillbar-bar" style="background:#2F3238;"></div>
+										<div class="skill-bar-percent">80%</div>
+									</div> <!-- End Skill Bar -->
+									
+									<div class="skillbar clearfix " data-percent="75%">
+										<div class="skillbar-title" style="background:#2F3238;"><span>CSS</span></div>
+										<div class="skillbar-bar" style="background:#2F3238;"></div>
+										<div class="skill-bar-percent">75%</div>
+									</div> <!-- End Skill Bar -->
+									
+									
+									
+									<div class="skillbar clearfix " data-percent="60%">
+										<div class="skillbar-title" style="background:#2F3238;"><span>JAVASCRIPT</span></div>
+										<div class="skillbar-bar" style="background:#2F3238;"></div>
+										<div class="skill-bar-percent">60%</div>
+									</div> <!-- End Skill Bar -->
+									
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-sm-3">
+									<h4>Contact</h4>
+								</div>
+								<div class="col-sm-9">
+									<a href="mailto:john@SLAMR.com">balaji.nsitd@gmail.com</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				
+					<div class="col-md-6">
+						<p>Hi my name is Balaji Palani. I'm a 21 year old multi talent from Chennai India.</p>							
+						<p>Allow me make you a short presentation of myself, with entirely the key facts. If you want to know more about me get in touch. <br>Thanks alot for visiting.</p><br>
+						Kind Regards,							
+						<img src="img/signature.png" class="img-responsive" alt="" />
+					</div>				
+			</div>
+		</div><!--container-->
+	</section>
+	
+	
+	
+	
+	
+	<section id="cv">
+		<div class="container">
+			<div class="row">
+				<div class="hero col-sm-6 col-sm-offset-3">
+					<h2>Curriculum Vitae</h2>
+					<p>Get to know where I've proven my skills</p>
+					<div class="space"></div>
+				</div>
+			</div>
+		</div>
+		<div class="container">
+			<div class="cv-wrapper">
+				<div class="row cv">
+					<div class="col-lg-3 title">
+						<h3 class="headline-inline">EDUCATION</h3>
+					</div>
+					<div class="cv-entry animated" data-animation-delay="100" data-animation="fadeInLeft">
+						<div class="col-lg-6 main">
+							<h4>Bachelor of Engineering</h4>
+							<i class="fa fa-building-o"></i> Netaji Subhas Institute of Technology, Delhi <small> (4 Years Course)</small>
+
+							
+							<hr>
+						</div>
+						<div class="col-lg-3 date">
+							<p>
+								<small><i class="fa fa-calendar-o"></i> August 2010 - May 2014</small>
+							</p>
+						</div>
+					</div>
+					<div class="cv-entry animated" data-animation-delay="150" data-animation="fadeInRight">
+						<div class="col-lg-6 col-lg-offset-3 main">
+							<h4>Senior School Grade 12 </h4>
+							<i class="fa fa-building-o"></i> Central Board of Secondary Education  <small> (1 Years Course)</small>
+							
+							<hr>
+
+							
+						</div>
+						<div class="col-lg-3 date">
+							<p>
+								<small><i class="fa fa-calendar-o"></i> June 2010</small>
+							</p>
+						</div>
+					</div>
+					<div class="cv-entry animated" data-animation-delay="150" data-animation="fadeInRight">
+						<div class="col-lg-6 col-lg-offset-3 main">
+							<h4>High School Grade 10 </h4>
+							<i class="fa fa-building-o"></i> Central Board of Secondary Education  <small> (1 Years Course)</small>
+
+							
+						</div>
+						<div class="col-lg-3 date">
+							<p>
+								<small><i class="fa fa-calendar-o"></i> June 2008</small>
+							</p>
+						</div>
+					</div>
+				</div>
+				
+				<div class="row cv">
+					<div class="col-lg-3 title">
+						<h3 class="headline-inline">WORK</h3>
+					</div>
+					<div class="cv-entry animated" data-animation-delay="100" data-animation="fadeInLeft">
+						<div class="col-lg-6 main">
+							<h4>Software Developer - Python</h4>
+							<i class="fa fa-building-o"></i> RatedApartments.
+
+							<p class="desc">
+								Reviewed and maintained the whole code of RatedApartments.com written in python deployed on Google App Engine using webapp2 framework and Google NDB Datastore. Designed and developed Crowdcapital.in sister concerned of RatedApartments Developed Macro for Accounts departments using VBA Designed and developed new model for ratedapartment.com using Python Django framework
+							</p>
+							
+						</div>
+						<div class="col-lg-3 date">
+							<p>
+								<small><i class="fa fa-calendar-o"></i> June 2014 - September 2014</small>
+							</p>
+						</div>
+					</div>
+			
+				</div>
+				
+			</div><!--cv wrapper-->
+
+			<p>
+				<a href="cv/balaji_p.pdf" class="btn btn-default btn-large"><i class="fa fa-download"></i> Download my CV</a>
+			</p>
+		</div>
+	</section>
+			
+	<section class="callToAction">			
+		<div class="container animated" data-animation-delay="200" data-animation="rubberBand">
+			<h2>I'm currently available for work.</h2>
+			<button class="btn btn-default btn-lg btn-toWhite"  onclick="$.scrollTo( '#contact', 800, {easing:'easeOutExpo', axis:'y'} );"><i class="fa fa-check"></i> HIRE ME</button>
+		</div>
+		
+		
+		
+		
+	</section>
+
+	
+	
+	
+	
+	
+
+	<section id="contact" class="no-section-padding">
+		
+		<div class="container text-center">
+			<div class="row section-padding">
+				
+				<div class="col-sm-6 col-sm-offset-3 animated" data-animation-delay="300" data-animation="fadeInLeft">
+					<h2>Balaji Palani </h2>
+					<address>
+						14/8 3rd Cross Street
+						<br>
+						MG Road, Pattabiram
+						<br>
+						Chennai, India, 600072
+						<br>
+						<abbr title="Phone">P:</abbr> (+91) 7838154873
+						<br>
+						<a href="mailto:#">balaji.nsitd@gmail.com</a>
+					</address>
+					<div class="background-icon left animated" data-animation-delay="400" data-animation="fadeInRight">
+						<i class="fa fa-envelope-o"></i>
+					</div>
+				</div>
+				
+			</div><!--row-->
+			
+				<div class="social-icons">
+					<a href="https://www.facebook.com/pppbalaji" target="_blank" class="facebook"><i class="fa fa-facebook"></i></a>
+					
+					<a href="http://in.linkedin.com/pub/balaji-palani/62/662/439/" target="_blank" class="linkedin"><i class="fa fa-linkedin"></i> </a>
+					
+					<a href="#" target="_blank" class="dropbox"><i class="fa fa-dropbox"></i> </a>
+					
+				</div>
+				<div class="clearfix"></div>
+			
+		</div><!--container-->		
+	</section>
+
+	<!--footer-->
+	<footer>
+	<div class="container text-center">
+		<div class="row">
+			<div class="col-md-12">
+				<p class="copy">
+					&copy; <strong>Balaji Palani</strong> 2014
+				</p>
+			</div>
+		</div>
+	</div>
+	</footer>
+
+  		<script type="text/javascript" src="js/jquery-1.10.1.min.js"></script>
+		<script type="text/javascript" src="js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="js/plugins.js"></script>
+		<script type="text/javascript" src="js/owl.carousel.min.js"></script>
+		<script type="text/javascript" src="js/main.js"></script>
+
+		<script>
+			$(document).ready(function(){		
+				SLAMR.preloader();		
+				SLAMR.nav();	
+				SLAMR.parallax(true);							
+				SLAMR.slider("#slides", {'play':5000, 'animation': 'fade'});				
+				SLAMR.skillbar("animated");	
+				SLAMR.slideDownHead(true);			
+				SLAMR.scrollAnim("yes");							
+				SLAMR.owlCarousel({
+				'#owl-carousel-blog':{singleItem : true,
+						lazyLoad:true,
+						autoPlay : true},
+				'#owl-carousel-clients':{navigation: true,
+				navigationText: [
+      				"<i class='fa fa-angle-left arrow-round'></i>",
+      				"<i class='fa fa-angle-right arrow-round'></i>"
+      			]},
+      			'#owl-carousel-testimonial':{singleItem:true,transitionStyle: "goDown",navigation: true,
+				navigationText: [
+      				"<i class='fa fa-angle-left arrow-round'></i>",
+      				"<i class='fa fa-angle-right arrow-round'></i>"
+      			]}, 
+				});	
+				SLAMR.bootstrapCarousel({
+				'#carousel-head':{interval:5000,pause:false}
+				});	
+				SLAMR.contactForm();
+				SLAMR.fitVids(".video-container");
+				
+				SLAMR.magnific({
+					'.image-modal':{type:'image',image: { 
+						markup: '<div class="mfp-figure">'+
+						            '<div class="mfp-close"></div>'+
+						            '<div class="mfp-img"></div>'+
+						            '<div class="mfp-bottom-bar">'+
+						              '<div class="mfp-title"></div>'+
+						              '<div class="mfp-counter"></div>'+
+						            '</div>'+
+						          '</div>',												  
+						  titleSrc: 'title',verticalFit: true,tError: '<a href="%url%">The image</a> could not be loaded.'
+					},closeBtnInside:true,midClick: true,fixedContentPos:false,preloader:true,gallery:{ enabled:true},removalDelay: 0,mainClass: 'mfp-fade'}});
+				
+				SLAMR.portfolioFilter("#portfolio .filter a", "#portfolio .portfolio-grid div");
+				SLAMR.portfolio(".portfolio-link",{
+					"pWrapper"	: ".portfolio-wrapper",
+		            "section"	: "#portfolio",
+		            "pContent"	: ".portfolio-content",
+		            "pContainer": ".portfolio-container",
+		            "close"		: ".portfolio-close",
+		            "carousel"	: ".owl-portfolio"
+		      	});							
+			}); //document ready
+		</script>
+		
+    </body>   
+</html>
